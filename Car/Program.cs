3ada80aa4ebd0cfce5636ee1097c6acea01c7e2d
Opilane TTHK <opilane@tthk.ee>
2020-11-26 13:10:18 +0200

    first

---
+++ Car/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+
+namespace Car
+{
+    class Program
+    {
+        class Car
+        {
+            string mark;
+            string model;
+            string registration;
+            string color;
+            int odometer;
+            int fuel;
+
+            public Car(string _mark, string _model, string _registration, string _color)
+            {
+                Mark = _mark;
+                Model = _model;
+                Registration = _registration;
+                Color = _color;
+                odometer = 0;
+                fuel = 60;
+            }
+            public void Drive()
+            {
+                odometer += 100;
+                fuel -= 5;
+            }
+            public string Registration
+            {
+                get { return registration; }
+                set
+                {
+                    if (value.Length >= 6)
+                    {
+                        registration = value;
+                    }
+                    else
+                    {
+                        registration = "undefined";
+                    }
+                }
+            }
+            public string Model
+            {
+                get { return model; }
+                set
+                {
+                    model = value;
+                }
+            }
+            public string Mark
+            {
+                get { return mark; }
+                set
+                {
+                    mark = value;
+                }
+            }
+            public string Color
+            {
+                get { return color; }
+                set
+                {
+                    color = value;
+                }
+            }
+            public int Fuel
+            {
+                get { return fuel; }
+                set
+                {
+                    fuel = value;
+                }
+            }
+            public int Odometer
+            {
+                get { return odometer; }
+                set
+                {
+                    odometer = value;
+                }
+            }
+            public void DataDisplay()
+            {
+                Console.WriteLine("mark: " + Mark);
+                Console.WriteLine("model: " + Model);
+                Console.WriteLine("reg: " + Registration);
+                Console.WriteLine("color: " + Color);
+                Console.WriteLine("fuel: " + fuel);
+                Console.WriteLine("odometer: " + odometer);
+            }
+        }
+        
+            
+        static void Main(string[] args)
+        {
+            Car newCar = new Car("bmw", "e46 compact", "777THC", "beige");
+
+            while (newCar.Fuel > 0)
+            {
+                newCar.Drive();
+                Console.WriteLine("fuel: " + newCar.Fuel);
+                Console.WriteLine("odometer: " + newCar.Odometer);
+            }
+            newCar.DataDisplay();
+        }
+    }
+}
---
+++ OPChallenges/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace OPChallenges
+{
+    class Program
+    {
+        class Dog
+        {
+            string name;
+            int spots;
+            double happiness;
+            
+            public Dog(string _name, int _spots)
+            {
+                name = _name;
+                spots = _spots;
+                happiness = 0;
+                Console.WriteLine("DEBUG, created name: " + name);
+            }
+            public double Happiness
+            {
+                get { return happiness; }
+            }
+            public void Barks()
+            {
+                Console.WriteLine("woof");
+                happiness += 0.2;
+            }
+            public void Wiggle()
+            {
+                Console.WriteLine("wiggle");
+            }
+            public void Rename(string NewName)
+            {
+                name = NewName;
+            }
+            public void DataDisplay()
+            {
+                Console.WriteLine("DEBUG, name: " + name);
+                Console.WriteLine("DEBUG, spots: " + spots);
+                Console.WriteLine("DEBUG, happiness: " + happiness);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Dog newDog = new Dog("põld", 10);
+            while (newDog.Happiness !< 1)
+            {
+                newDog.Barks();
+                Console.WriteLine("DEBUG, Happiness: " + newDog.Happiness);
+            }
+            newDog.Rename("attention retard, write a new name" + Console.ReadLine());
+            newDog.DataDisplay();
+        }
+    }
+}
